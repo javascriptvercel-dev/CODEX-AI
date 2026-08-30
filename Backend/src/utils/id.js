75a92dc76ce54a3d786c03820232ef7ae99341d9
blaze910 <dangogodspower@gmail.com>
2026-08-30 21:51:11 +0100

    Version 5.5

--- Backend/src/utils/id.js
+++ Backend/src/utils/id.js
@@ -3,4 +3,4 @@ const alphabet =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 const nanoid = customAlphabet(alphabet, 20);
 export const generatePublicId = () => nanoid();
-export const buildInstallCommand = (publicId) => `.plugin ${publicId}`;
+export const buildInstallCommand = (publicId) => `.install ${publicId}`;
